0a461fec7d73d8a40cb504a4c50f8b1a06676369
dev-jaemin <jay.kimjaemin@gmail.com>
2022-07-27 23:30:46 +0900

    mainpage 수정

--- src/page/intro/Intro.js
+++ src/page/intro/Intro.js
@@ -4,13 +4,9 @@ import "./Info.css";
 const Intro = () => {
     return (
         <div className="intro_wrapper">
-            <div className="intro_section">
-                <div className="logo">SOOLTHER TOWN</div>
-
-                <Link to="/info">
-                    <button className="cta_button">시작하기</button>
-                </Link>
-            </div>
+            <Link to="/info">
+                <button className="cta_button">START</button>
+            </Link>
         </div>
     );
 };
